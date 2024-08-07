b4ab848b40fcee04827655eeec7e686926bad058
soerenlemke <soerenlemke@gmx.de>
2024-08-07 18:30:39 +0200

    formating & cleanup

--- src/loader.rs
+++ src/loader.rs
@@ -10,12 +10,12 @@ pub fn read_file_to_string(filename: &str) -> Result<String, io::Error> {
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    
+
     Ok(contents)
 }
 
 pub fn load_yaml_file(filename: &str) -> Result<YAMLData, YAMLParseError> {
     let contents = read_file_to_string(filename).map_err(|_| YAMLParseError::InvalidFormat)?;
-    
+
     parse_yaml(&contents)
 }
